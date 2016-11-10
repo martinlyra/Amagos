f5ee70b12c2c8a30983262b4721086bdacab84c1
Martin Lyrå <martin.lyra.97@gmail.com>
2016-11-10 18:59:29 +0100

    Fixed a Common.targets issue.

--- Amagos/Properties/AssemblyInfo.cs
+++ Amagos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Amagos")]
+[assembly: AssemblyTitle("Amagosa")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Amagos")]
+[assembly: AssemblyProduct("Amagosa")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3af5644d-8c92-41c4-b7bb-865f57a0e4cf")]
+[assembly: Guid("753a3951-9d85-4614-9a4e-4a414fcf146b")]
 
 // Version information for an assembly consists of the following four values:
 //
